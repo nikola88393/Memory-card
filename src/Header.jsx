f8d71a380dadec7d0b0877e02ddfa6d8e633ef7d
Nikola <nikola88393@gmail.com>
2024-07-24 14:01:26 +0300

    Remove unnecessary state and effect from Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,37 +1,28 @@
-import { useEffect, useState } from "react";
-
 /* eslint-disable react/prop-types */
 const Header = ({ score, bestScore, getDifficulty }) => {
-  const [difficulty, setDifficulty] = useState(6);
-
-  useEffect(() => {
-    if (getDifficulty) {
-      getDifficulty(difficulty);
-    }
-  }, [difficulty, getDifficulty]);
-
   const handleDifficultyChange = (event) => {
-    setDifficulty(event.target.value);
+    getDifficulty(event.target.value);
   };
 
   return (
     <header>
-      <h1>Memory game</h1>
+      <div>
+        <h1>Memory game</h1>
+        <label htmlFor="difficulty">Choose a difficulty:</label>
+        <select
+          id="difficulty"
+          name="difficulty"
+          onChange={handleDifficultyChange}
+        >
+          <option value={6}>Easy</option>
+          <option value={10}>Medium</option>
+          <option value={12}>Hard</option>
+        </select>
+      </div>
       <div className="scoreContainer">
         <p>Score: {score}</p>
         <p>Best Score: {bestScore}</p>
       </div>
-      <label htmlFor="difficulty">Choose a difficulty:</label>
-      <select
-        id="difficulty"
-        name="difficulty"
-        value={difficulty}
-        onChange={handleDifficultyChange}
-      >
-        <option value={6}>Easy</option>
-        <option value={10}>Medium</option>
-        <option value={12}>Hard</option>
-      </select>
     </header>
   );
 };
