deca5a8572e1fc3c6da5ee28c5930b99e3b49140
Nikola <nikola88393@gmail.com>
2024-07-24 13:17:38 +0300

    Implement score reset if difficulty is changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Game from "./Game";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [difficulty, setDifficulty] = useState(6);
+
+  useEffect(() => {
+    setScore(0);
+    setBestScore(0);
+  }, [difficulty]);
 
   const incrementScore = () => {
     setScore(score + 1);
